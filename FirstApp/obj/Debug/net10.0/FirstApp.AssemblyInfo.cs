49269c83f2318e2eef1c65bb3a97dab7fdcdbad0
Kishan Lal <kishankumar969846@gmail.com>
2025-12-26 09:28:19 +0530

    Updated notes and build files

--- FirstApp/obj/Debug/net10.0/FirstApp.AssemblyInfo.cs
+++ FirstApp/obj/Debug/net10.0/FirstApp.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("FirstApp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+31f954e75aac668920ef36917094981fe45adf5b")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c49913b419b401b24eb90878ca01e474ac5d1d5e")]
 [assembly: System.Reflection.AssemblyProductAttribute("FirstApp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("FirstApp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]

701e8150a13f087cad0fc70f64d12e03bcfe000b
Kishan Lal <kishankumar969846@gmail.com>
2026-01-17 14:25:55 +0530

    saturday

--- C#/SaturdayAssignment/MovieLibrary/Program.cs
+++ C#/SaturdayAssignment/MovieLibrary/Program.cs
@@ -1,37 +1,37 @@
-﻿// using System;
-// using MovieLibraryApp.Models;
-// using MovieLibraryApp.Library;
+﻿using System;
+using MovieLibraryApp.Models;
+using MovieLibraryApp.Library;
 
-// namespace MovieLibraryApp
-// {
-//     class Program
-//     {
-//         static void Main(string[] args)
-//         {
-//             FilmLibrary library = new FilmLibrary();
+namespace MovieLibraryApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            FilmLibrary library = new FilmLibrary();
 
-//             library.AddFilm(new Film("Inception", "Christopher Nolan", 2010));
-//             library.AddFilm(new Film("Interstellar", "Christopher Nolan", 2014));
-//             library.AddFilm(new Film("3 Idiots", "Rajkumar Hirani", 2009));
+            library.AddFilm(new Film("Inception", "Christopher Nolan", 2010));
+            library.AddFilm(new Film("Interstellar", "Christopher Nolan", 2014));
+            library.AddFilm(new Film("3 Idiots", "Rajkumar Hirani", 2009));
 
-//             Console.WriteLine("🎬 All Films:");
-//             Display(library.GetFilms());
+            Console.WriteLine("🎬 All Films:");
+            Display(library.GetFilms());
 
-//             Console.WriteLine("\n🔍 Search Result for 'Nolan':");
-//             Display(library.Search("Nolan"));
+            Console.WriteLine("\n🔍 Search Result for 'Nolan':");
+            Display(library.Search("Nolan"));
 
-//             library.RemoveFilm("3 Idiots");
+            library.RemoveFilm("3 Idiots");
 
-//             Console.WriteLine("\n📂 Films After Removal:");
-//             Display(library.GetFilms());
-//         }
+            Console.WriteLine("\n📂 Films After Removal:");
+            Display(library.GetFilms());
+        }
 
-//         static void Display(System.Collections.Generic.List<Film> films)
-//         {
-//             foreach (var film in films)
-//             {
-//                 Console.WriteLine($"Title: {film.Title}, Director: {film.Director}, Year: {film.Year}");
-//             }
-//         }
-//     }
-// }
+        static void Display(System.Collections.Generic.List<Film> films)
+        {
+            foreach (var film in films)
+            {
+                Console.WriteLine($"Title: {film.Title}, Director: {film.Director}, Year: {film.Year}");
+            }
+        }
+    }
+}
